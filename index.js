6a53c2f3a1671caaa733a37269a7a647f45e835d
Vedanti.U <vedanti.vvu@gmail.com>
2024-03-25 20:40:45 +0530

    ai-integration successful

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const { ipcRenderer } = require('electron')
 var ipc = ipcRenderer;
-console.log(ipc.ipcRenderer)
-var term = new Terminal();
+
+let term = new Terminal({
+
+
+  theme:{
+    "foreground": "#ebeef5",
+    "background": "#1d2935",
+    "cursor": "#e6a23c",
+    "black": "#000000",
+    "brightBlack": "#555555",
+    "red": "#ef4f4f",
+    "brightRed": "#ef4f4f",
+    "green": "#67c23a",
+    "brightGreen": "#67c23a",
+    "yellow": "#e6a23c",
+    "brightYellow": "#e6a23c",
+    "blue": "#409eff",
+    "brightBlue": "#409eff",
+    "magenta": "#ef4f4f",
+    "brightMagenta": "#ef4f4f",
+    "cyan": "#17c0ae",
+    "brightCyan": "#17c0ae",
+    "white": "#bbbbbb",
+    "brightWhite": "#ffffff"
+  }
+,
+  fontSize: 24,
+  fontFamily: 'Ubuntu Mono, courier-new, courier, monospace'
+
+});
 term.open(document.getElementById('terminal'));
 
 ipc.on("terminal.incomingData", (event, data) => {
-
+    console.log(data)
     term.write(data);
 });
 
+ipc.on("terminal.reset", (event, data) => {
+    console.log(data)
+    term.clear();
+});
 term.onData(e => {
-    console.log(e)
-    //ipc.send(ls);
     ipc.send("terminal.keystroke", e);
 });
